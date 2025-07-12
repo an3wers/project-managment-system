0fad44a52c15fa7e1b2da524287866ff4f64bde9
an3wers <an3wer@yandex.ru>
2025-07-12 22:33:06 +0500

    add: task page

--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { usePageStore } from '@/stores/page-store'
 import { projectQuery } from '@/utils/supabase/queryes'
 import type { Project } from '@/utils/supabase/queryes'
 
-const route = useRoute()
+const route = useRoute('/projects/[slug]')
 const { setPageData } = usePageStore()
 
 const project = ref<Project | null>(null)
+
+watch(
+  () => project.value?.name,
+  () => {
+    setPageData({ title: project.value?.name ?? 'Project' })
+  },
+)
 
 const getProject = async () => {
   const { data, error } = await projectQuery(route.params.slug as string)
@@ -17,16 +24,17 @@ const getProject = async () => {
     console.error(error)
   }
 
-  project.value = data?.[0] ?? null
+  project.value = data ?? null
 }
 
 await getProject()
-
-setPageData({ title: project.value?.name ?? 'Project' })
 </script>
 
 <template>
   <div>
-    {{ project }}
+    <p>{{ project?.description }}</p>
+    <p>{{ project?.status }}</p>
+    <p>{{ project?.created_at }}</p>
+    <p>{{ project?.tasks }}</p>
   </div>
 </template>
